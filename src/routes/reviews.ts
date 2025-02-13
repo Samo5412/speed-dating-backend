86b79fd884a7aa87e874fc75da84e17387529384
Joakim Colloz <joco2201@student.miun.se>
2025-02-13 10:33:23 +0100

    Add review routes
    Added initial review routes for CRUD operations in the routes folder.

---
+++ src/routes/reviews.ts
@@ -0,0 +1,18 @@
+import express, { Router } from "express";
+import { 
+  createReview,
+  getReview,
+  getReviews,
+  updateReview,
+  deleteReview
+} from "../controllers/reviews.js";
+
+const router: Router = express.Router();
+
+router.post("/", createReview);
+router.get("/", getReviews);
+router.get("/:id", getReview);
+router.put("/:id", updateReview);
+router.delete("/:id", deleteReview);
+
+export const reviewsRouter = router;
