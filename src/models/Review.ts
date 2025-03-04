bd32d574c7e14b3089b6b5ea17e5feb857f077b4
Josef Alirani <joal2203@student.miun.se>
2025-03-04 11:19:10 +0100

    lagt till ny endpoint till reviews, som hämtar alla reviews från en runda i ett event

--- src/models/Review.ts
+++ src/models/Review.ts
@@ -13,8 +13,20 @@ const ReviewSchema = new mongoose.Schema({
     required: true,
     index: true,
   },
+  eventId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Event",
+    required: true,
+    index: true,
+  },
+  round: {
+    type: Number, required: true
+  },
   rating: { type: Number, min: 1, max: 5, required: true },
   comment: { type: String },
+  showedUp: {type: Boolean},
+  sameTable: {type:Boolean},
+  sitNextTo: {type:Boolean},
   createdAt: { type: Date, default: Date.now },
 });
 
